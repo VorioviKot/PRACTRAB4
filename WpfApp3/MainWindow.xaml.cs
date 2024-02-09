1ef47d8bf90aa7f2d87b3b5d1839a62498083cfe
Student <Student@417-EKZ-02>
2024-02-09 12:21:11 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }    
+        /// <summary>
+        /// open in menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("u open");
+        }
+        /// <summary>
+        /// seve in menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("u seve");
+        }
+        /// <summary>
+        /// close in menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        /// <summary>
+        /// about in menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+        /// <summary>
+        /// seve in menu типа пометки при наведении
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// рисование 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItem_Click_5(object sender, RoutedEventArgs e)
+        {
+            InkCanvas1.EditingMode = InkCanvasEditingMode.Ink;
+        }
+        /// <summary>
+        /// ркдактор
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItem_Click_6(object sender, RoutedEventArgs e)
+        {
+            InkCanvas1.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+        /// <summary>
+        /// удаление 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItem_Click_7(object sender, RoutedEventArgs e)
+        {
+            InkCanvas1.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+    }
+}
